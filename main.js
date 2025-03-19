5e259fed5310f2f919fba4521003f9c4cdeaf8cb
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-03-19 15:31:38 -0500

    Added some new styling to the calculator and improved the logic

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const buttons = document.querySelectorAll("button");
 const textBox = document.querySelector("input");
 
-let operand1 = null;
-let operand2 = null;
-let operator1 = null;
-let operator2 = null;
-let textBoxVal = 0;
-let result = 0;
+let number1 = null;
+let number2 = null;
+let operation = null;
+let result = null;
+let displayOutput = 0;
 
 function add(val1, val2) {
   return Number(val1) + Number(val2);
@@ -36,7 +35,7 @@ function operate(operation, num1, num2) {
     case "*":
       return mult(num1, num2);
     case "/":
-      return div(num1, num2);
+      return div(num1, num2).toFixed(2);
     default:
       return "Invalid operation";
   }
@@ -46,36 +45,60 @@ buttons.forEach((button) => {
   button.addEventListener("click", (event) => handleInput(event));
 });
 
-function handleDigits() {}
-
-function handleOperators() {}
-
-function handleEqual() {}
+function handleDigits(value) {
+  // Need to handle the case where result is not null
+  if (!operation && !number1 && !number2 && !result) {
+    number1 = value;
+    displayOutput = number1;
+  } else if (number1 && !operation && !number2 && !result) {
+    number1 += value;
+    displayOutput = number1;
+  } else if (number1 && operation && !number2 && !result) {
+    number2 = value;
+    displayOutput = number2;
+  } else if (number1 && operation && number2 && !result) {
+    number2 += value;
+    displayOutput = number2;
+  } else if(result){
+    number1 = result;
+    number2 = value;
+    result = null;
+    displayOutput = number2;
+  }
+  textBox.value = displayOutput;
+}
 
 function handleClear() {
-  operand1 = null;
-  operand2 = null;
-  operator1 = null;
-  operator2 = null;
-  textBoxVal = 0;
-  result = 0;
+  number1 = null;
+  number2 = null;
+  operation = null;
+  result = null;
+  displayOutput = 0;
+  textBox.value = displayOutput;
 }
 
 function handleInput(event) {
   const valueType = event.target.className;
+  const value = event.target.textContent;
 
   switch (valueType) {
     case "digits":
       console.log("Button is a digit");
+      handleDigits(value);
       break;
     case "operator":
       console.log("Button is a operator");
+      operation = value;
       break;
     case "equal":
       console.log("Button is a equal");
+      result = operate(operation, number1, number2);
+      displayOutput = result;
+      textBox.value = displayOutput;
       break;
     case "clear":
       console.log("Button is a clear");
+      handleClear();
       break;
     default:
       console.log("Idk");
